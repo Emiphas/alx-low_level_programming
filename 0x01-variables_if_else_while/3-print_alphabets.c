e895819000ac5d9e3ca658f74445792730bc3d85
Emiphas <emiphas@gmail.com>
2023-06-16 12:25:59 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'd') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
