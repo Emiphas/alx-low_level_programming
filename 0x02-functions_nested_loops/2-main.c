5d57e9e52a4b9d4a66d91c215255e5ec5d7c1f5e
Emiphas <emiphas@gmail.com>
2023-06-20 09:41:14 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
