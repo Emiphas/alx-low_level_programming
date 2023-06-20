bdf096417febf182a523bc4b60268a6004ba342e
Emiphas <emiphas@gmail.com>
2023-06-20 13:30:27 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * n1: first number to be entered
+ * n2: second number to be entered
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
